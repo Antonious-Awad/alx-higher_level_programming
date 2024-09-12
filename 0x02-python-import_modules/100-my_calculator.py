6a8462166164f2e17eda232651fca02275b6539d
Antonious-Awad <antonuostony1@gmail.com>
2024-09-12 14:39:09 +0300

    chore: 2.100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,24 +6,16 @@ if __name__ == "__main__":
     argc = len(sys.argv)
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
     operator = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    result = 0
+    operations = {"+": calc.add, "-": calc.sub, "*": calc.mul, "/": calc.div}
 
-    match operator:
-        case "+":
-            result = calc.add(a, b)
-        case "*":
-            result = calc.mul(a, b)
-        case "/":
-            result = calc.div(a, b)
-        case "-":
-            result = calc.sub(a, b)
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    if operator not in list(operations.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-    print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
+    print("{:d} {} {:d} = {:d}".format(
+        a, operator, b, operations[operator](a, b)))
