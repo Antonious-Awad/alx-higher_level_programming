be98c1820b67f2692cd5a118b610b3732c7a968e
Antonious-Awad <antonuostony1@gmail.com>
2024-09-18 13:42:47 +0300

    chore:4.12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def sub_roman(a, b):
+    # to cater for already added value
+    return a - (b*2)
+
+
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev_dig = 0
+    result = 0
+
+    for r in roman_string:
+        if r in romans:
+            if romans[r] > prev_dig:
+                result += sub_roman(romans[r], prev_dig)
+            else:
+                result += romans[r]
+            prev_dig = romans[r]
+
+    return result
