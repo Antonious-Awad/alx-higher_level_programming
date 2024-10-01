613bd51b7483f53c7bb1cebf4b19a8b2860224dd
Antonious-Awad <antonuostony1@gmail.com>
2024-10-01 16:22:48 +0300

    chore: 6.1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Defining A square class """
+
+
+class Square:
+    """ Square """
+
+    def __init__(self, size):
+        """ instantiate a square 
+            Args:
+              size (int): Size of the square
+        """
+        self.__size = size
