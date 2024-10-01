0a4d2abba23d34ab2cd425388160e998096546ee
Antonious-Awad <antonuostony1@gmail.com>
2024-10-01 17:09:14 +0300

    chore: 6.5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,15 +5,21 @@
 class Square:
     """ Square """
 
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0):
         """initialize square
 
         Args:
             size (int): size of the square
-            position (int, int): position in a cartesian plane
         """
-        self.size = size
-        self.position = position
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Gets the area of square"""
+        return self.__size * self.__size
 
     @property
     def size(self):
@@ -29,40 +35,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
-    def position(self):
-        """ getter for position attribute """
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        """position setter
-
-        Args:
-            value (tuple): value to be set
-
-        Raises:
-            TypeError: position must be a tuple of 2 +ve integers
-        """
-        if (not isinstance(value, tuple)
-            or len(value) != 2
-            or not all(isinstance(x, int) for x in value)
-                or all(num < 0 for num in value)):
-            raise TypeError(
-                "position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def area(self):
-        """Gets the area of square"""
-        return self.__size * self.__size
-
     def my_print(self):
         """ prints a square"""
         if self.__size == 0:
             print()
             return
-        for i in range(self.position[1]):
-            print()
         for i in range(self.__size):
-            print(" " * self.position[0], end="")
             print("#" * self.__size)
