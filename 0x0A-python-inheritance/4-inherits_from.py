61978d1ccce102e67e8491372dba001fffd952b6
Antonious-Awad <antonuostony1@gmail.com>
2024-10-14 14:17:05 +0300

    chore: 0A.4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     class that inherited (directly or indirectly) 
     from the specified class
     """
-    return (issubclass(obj, a_class) and type(obj) != a_class)
+    return (isinstance(obj, a_class) and type(obj) != a_class)
