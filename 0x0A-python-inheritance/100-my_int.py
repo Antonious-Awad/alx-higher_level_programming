eef70346224bda88f8239cfaee7701914ed5436b
Antonious-Awad <antonuostony1@gmail.com>
2024-10-14 21:34:05 +0300

    chore: 0A.12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""MyInt Module Implementation"""
+"""Module for Defining a class MyInt that inherits from int."""
 
 
 class MyInt(int):
+    """Invert operators == and !=."""
+
     def __eq__(self, value):
-        """equal operation
-
-        Args:
-            value (int): value
-
-        Returns:
-            boolean: result of comparison
-        """
+        """Override == operator with != behavior."""
         return self.real != value
 
     def __ne__(self, value):
-        """not equal operation
-
-        Args:
-            value (int): value
-
-        Returns:
-            boolean: result of comparison
-        """
+        """Override != operator with == behavior."""
         return self.real == value
