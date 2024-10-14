d3ade699fb5d5519719aa910d621e2a9f570ec33
Antonious-Awad <antonuostony1@gmail.com>
2024-10-14 14:18:16 +0300

    chore: 0A.4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,9 +4,8 @@
 
 
 def inherits_from(obj, a_class):
-    """
-    checks if the object is an instance of a 
-    class that inherited (directly or indirectly) 
+    """checks if the object is an instance of a
+    class that inherited (directly or indirectly)
     from the specified class
     """
-    return (isinstance(obj, a_class) and type(obj) != a_class)
+    return (isinstance(obj, a_class) and type(obj) is not a_class)
