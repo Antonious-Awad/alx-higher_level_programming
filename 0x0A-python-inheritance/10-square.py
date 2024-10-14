265e55005aaacbd9aef18ed17c55c1ab65b2f373
Antonious-Awad <antonuostony1@gmail.com>
2024-10-14 21:23:14 +0300

    chore: 0A.10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,8 +5,18 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Square
+
+    Args:
+        Rectangle: Rectangle Parent
+    """
 
     def __init__(self, size):
+        """Instantiation
+
+        Args:
+            size (int): size of square
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
